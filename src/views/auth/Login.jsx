4837c37688030972fffebd67ff7af61217ac539f
Juanco <juanffo874@gmail.com>
2025-03-13 22:06:08 -0600

    Instala react-router y configura las primeras rutas

---
+++ src/views/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <p>Desde el login</p>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/views/public/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p>
+            Desde el home
+        </p>
+    </div>
+  )
+}
+
+export default Home
