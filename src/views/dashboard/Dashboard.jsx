3cc88abb7b86dfebb897e318098334f4cf5aef24
Juanco <juanffo874@gmail.com>
2025-04-17 13:29:27 -0600

    Modifica el sidebard y navbar para que puedan alinearse

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Outlet } from "react-router";
 
 //Importar componentes
-import Sidebard from './components/home/Sidebard'
-import NavBar from './components/home/NavBar'
+import Sidebard from "./components/home/Sidebard";
+import NavBar from "./components/home/NavBar";
 
 const Dashboard = () => {
-
   return (
     <>
-      <Sidebard/>
-      <NavBar/>
-      <div className="p-4 sm:ml-64">
-        <Outlet/>
+      <div className="app-layout flex h-screen">
+        <aside className="w-64 p-4 border">
+          <Sidebard />
+        </aside>
+
+        <div className="flex-1 flex flex-col">
+          <NavBar />
+          <main className="flex-1 p-4">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/views/dashboard/components/home/NavBar.jsx
+++ src/views/dashboard/components/home/NavBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      <header className="w-full bg-gray-50 px-6 py-4 flex items-center justify-between">
+        <input
+          type="text"
+          placeholder="Buscar transacciones..."
+          className="w-1/3 px-4 py-2 rounded-md border text-sm focus:outline-none focus:ring-2 focus:ring-green-500"
+        />
+        <div className="flex items-center gap-4">
+          {/* <div className="text-sm font-medium text-gray-700">Marzo 2025</div>
+          <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition">
+            Descargar
+          </button> */}
 
-export default NavBar
+          {/* Icono de notificación */}
+          {/* <button className="text-gray-500 hover:text-gray-700">
+          <Bell className="w-5 h-5" />
+        </button> */}
+
+          {/* Avatar de usuario
+        <button className="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden">
+          <UserCircle2 className="w-7 h-7 text-gray-600" />
+        </button> */}
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
