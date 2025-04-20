1897e1c7a9df8a4b49c3c7ddb9add5da639168e6
Juanco <juanffo874@gmail.com>
2025-04-20 00:25:13 -0600

    Instala nuevos paquetes para los modales e iconos

--- src/views/dashboard/views/Budget.jsx
+++ src/views/dashboard/views/Budget.jsx
@@ -4,8 +4,12 @@ const Budget = () => {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold">Presupuestos</h1>
-        
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold">Presupuestos</h1>
+          <button className="bg-green-700 text-gray-100 px-3 py-2 rounded-lg mt-2 hover:bg-green-600">            
+            Crear Nuevo Presupuesto
+          </button>
+        </div>
       </div>
     </>
   )
